537be5f8ca82c2e31c21de89dc4110ea231e687a
ShunOmine <shun12baseball@gmail.com>
2019-04-01 21:19:04 +0900

    edit layoutjs and index.css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,10 +3,6 @@ import Header from './Header'
 import Footer from './Footer'
 
 export default class Layout extends React.Component{
-    constructor(){
-        super();
-        this.name = "Mine";
-    }
     render(){
         return(
             <div>
@@ -14,7 +10,6 @@ export default class Layout extends React.Component{
                 <div className="wrapper">
                     <div className="container">
                         <h1>Welcome to React Sample App!</h1>
-                        <h1>It's {this.name} !</h1>
                     </div>
                 </div>
                 <Footer/>
